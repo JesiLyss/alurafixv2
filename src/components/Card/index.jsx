3227f55721a414135eb7678441cf6081fc864cf9
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-07-03 19:38:48 -0300

    add db.json file + img files simulating iframe

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,18 @@
 import Image from '../Image';
-import Title from "../Title";
+import Title from '../Title';
 import Button from './Button';
 import './Card.modules.css';
 
-function Card(){
+function Card({id, title, desc, url}){
     return (
     <>
         <div className="card">
             <div className="card__img__container">
-                <Image alt="card texto alternativo" img="../assets/img/dogs-main.webp" styles="card__img" />
+                <Image alt={desc} img={url} styles="card__img" />
             </div>
             <div className="card__body">
-                <Title type="h4" content="Nombre Card" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt odit, ad cupiditate aut et harum reprehenderit quas laboriosam non, esse alias fuga iusto quaerat.</p>
+                <Title type="h4" content={title} styles="card__title" />
+                <p>{desc}</p>
             </div>
             <div className="card__btn__container">
                 <Button url="" content="Edit" styles="edit" />
