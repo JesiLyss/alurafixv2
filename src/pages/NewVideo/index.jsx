bfc9f6b3a56737fbb7c334e27c53e3728de8ccc9
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-07-06 15:04:15 -0300

    added logic and use of states to store values to be used to create the new video. modification in form to update values(states) of inputs

--- src/pages/NewVideo/index.jsx
+++ src/pages/NewVideo/index.jsx
@@ -6,23 +6,79 @@ import Main from "../../components/Main";
 import Title from "../../components/Title";
 import Input from "../../components/Input";
 import Button from "../../components/Card/Button";
+import Select from "../../components/Select";
+import { useEffect, useState } from "react";
 
 function NewVideo(){
+
+    // const [id, setId] = useState("");
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
+    const [url, setUrl] = useState("");
+    const [image, setImage] = useState("");
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/categories')
+        .then(response => response.json())
+        .then(data => setCategories(data));
+    }, []);
+
+    const handleAdd = () => {
+        const newVideo = {
+            "title": title,
+            "description": description,
+            "category": category,
+            "url": url,
+            "image": image
+        };
+
+        console.log(newVideo);
+
+        fetch("http://localhost:3000/videos",{
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newVideo)
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
+        cleanValues();
+    };
+
+    const cleanValues = () => {
+        setTitle("");
+        setDescription("");
+        setCategory("");
+        setUrl("");
+        setImage("");
+    }
+
     return(
         <>
             <Header />
             <Main 
                 mainContent={
-                    <Form className="form" action="/insert" method="POST"
+                    <Form event={handleAdd} className="form"
                         content={
                             <>
+                            
                             <Title type="h4" content="Nuevo Video" />
-                            <Input placeholder="Ingrese el título del video" label="Título:" id="title" />
-                            <Input placeholder="Ingrese la url del video" label="Dirección:" id="direction" />
-                            <Input placeholder="Ingrese la descripción del video" label="Descripción:" id="description" />
+                            
+                            <Input type="text" placeholder="Ingrese el título del video" label="Título:" id="title" value={title} event={(e)=> setTitle(e.target.value)} />
+                            
+                            <Select label="Categorías" options={categories} selectID="category" value={category} event={(e)=> setCategory(e.target.value)} />
+                            
+                            <Input type="text" placeholder="Ingrese la descripción del video" label="Descripción:" id="description" value={description} event={(e)=> setDescription(e.target.value)} />
+                            
+                            <Input type="text" placeholder="Ingrese la url del video" label="Dirección:" id="direction" value={url} event={(e)=> setUrl(e.target.value)} />
+                            
+                            <Input type="text" placeholder="Ingrese la imagen del video" label="Imagen:" id="videoimg" value={image} event={(e)=> setImage(e.target.value)} />
+
                             <div className="btn__container">
-                                <Button content="Agregar" styles="add" />
-                                <Button content="Limpiar" styles="clean" />
+                                <Button type="submit" content="Agregar" styles="add" />
+                                <Button type="button" content="Limpiar" styles="clean" event={cleanValues} />
                             </div>
                             </>
                         }
@@ -30,7 +86,7 @@ function NewVideo(){
                 }
             />
             <Footer />
-        </>
+    </>
     )
 }
 
